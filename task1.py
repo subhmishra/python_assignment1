ea20303e2859bce8a1c9571e78ccb7f7ddd4dafa
Shubham Mishra <83873389+subhmishra@users.noreply.github.com>
2025-04-19 15:51:29 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,24 @@
+                           # Task 1: Perform Basic Mathematical Operations
+
+firstnum = float(input("Enter first number: "))
+secondnum = float(input("Enter other number: "))
+
+addition = firstnum+secondnum
+subtraction = firstnum - secondnum
+multiply = firstnum * secondnum
+divide = firstnum / secondnum
+
+print("Addition:",addition)
+print("subtraction:",subtraction)
+print("Multiplication:",multiply)
+print("Division:",divide)
+
+
+# Output
+# Enter first number: 100
+# Enter other number: 200
+# Addition: 300.0
+# subtraction: -100.0
+# Multiplication: 20000.0
+# Division: 0.5
+
---
+++ task2.py
@@ -0,0 +1,11 @@
+                                 # Task 2: Create a Personalized Greeting
+
+first_name =(input("enter your first name:"))
+last_name =(input("enter your last name:"))
+print("hellow, " +str(first_name)+ " " +str(last_name)+"! " "Welcome to the Python programme.")
+
+
+# output
+# enter your first name:chiki
+# enter your last name:mishra
+ # hellow, chiki mishra! Welcome to the Python programme.
